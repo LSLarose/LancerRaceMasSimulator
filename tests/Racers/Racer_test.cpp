46b7324cc53ceed679afd297fc568552aa995a94
Louis Sérey Larose <louiss.larose@gmail.com>
2022-06-23 23:29:57 -0400

    added tests for racer for min and max values on getters

--- tests/Racers/Racer_test.cpp
+++ tests/Racers/Racer_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "../../src/Racers/Racer.h"
 
-const int HASE_TEST_VAL = 3;
+const int HASE_TEST_VAL = MAX_HASE_BONUS/2;
 
 TEST(TestGetters, Hull_Normal) {
     Racer racer = Racer(Personality::unavailable, HASE_TEST_VAL,0,0,0);
@@ -9,10 +9,33 @@ TEST(TestGetters, Hull_Normal) {
     EXPECT_EQ(racer.getHull(), HASE_TEST_VAL);
 }
 
+TEST(TestGetters, Hull_Min) {
+    Racer racer = Racer(Personality::unavailable, MIN_HASE_BONUS-HASE_TEST_VAL, 0, 0, 0);
+
+    EXPECT_EQ(racer.getHull(), MIN_HASE_BONUS);
+}
+
+TEST(TestGetters, Hull_Max) {
+    Racer racer = Racer(Personality::unavailable, MAX_HASE_BONUS+HASE_TEST_VAL, 0, 0, 0);
+
+    EXPECT_EQ(racer.getHull(), MAX_HASE_BONUS);
+}
+
 TEST(TestGetters, Agility_Normal) {
     Racer racer = Racer(Personality::unavailable, 0, HASE_TEST_VAL, 0, 0);
 
     EXPECT_EQ(racer.getAgility(), HASE_TEST_VAL);
+}
+TEST(TestGetters, Agilityl_Min) {
+    Racer racer = Racer(Personality::unavailable, 0, MIN_HASE_BONUS - HASE_TEST_VAL, 0, 0);
+
+    EXPECT_EQ(racer.getAgility(), MIN_HASE_BONUS);
+}
+
+TEST(TestGetters, Agility_Max) {
+    Racer racer = Racer(Personality::unavailable, 0, MAX_HASE_BONUS + HASE_TEST_VAL, 0, 0);
+
+    EXPECT_EQ(racer.getAgility(), MAX_HASE_BONUS);
 }
 
 TEST(TestGetters, Systems_Normal) {
@@ -21,10 +44,34 @@ TEST(TestGetters, Systems_Normal) {
     EXPECT_EQ(racer.getSystems(), HASE_TEST_VAL);
 }
 
+TEST(TestGetters, Systems_Min) {
+    Racer racer = Racer(Personality::unavailable, 0, 0, MIN_HASE_BONUS - HASE_TEST_VAL, 0);
+
+    EXPECT_EQ(racer.getSystems(), MIN_HASE_BONUS);
+}
+
+TEST(TestGetters, Systems_Max) {
+    Racer racer = Racer(Personality::unavailable, 0, 0, MAX_HASE_BONUS + HASE_TEST_VAL, 0);
+
+    EXPECT_EQ(racer.getSystems(), MAX_HASE_BONUS);
+}
+
 TEST(TestGetters, Engineering_Normal) {
     Racer racer = Racer(Personality::unavailable, 0, 0, 0, HASE_TEST_VAL);
 
     EXPECT_EQ(racer.getEngineering(), HASE_TEST_VAL);
+}
+
+TEST(TestGetters, Engineering_Min) {
+    Racer racer = Racer(Personality::unavailable, 0, 0, 0, MIN_HASE_BONUS - HASE_TEST_VAL);
+
+    EXPECT_EQ(racer.getEngineering(), MIN_HASE_BONUS);
+}
+
+TEST(TestGetters, Engineering_Max) {
+    Racer racer = Racer(Personality::unavailable, 0, 0, 0, MAX_HASE_BONUS + HASE_TEST_VAL);
+
+    EXPECT_EQ(racer.getEngineering(), MAX_HASE_BONUS);
 }
 
 TEST(TestSetters, Hull) {
